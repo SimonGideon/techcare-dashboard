9685accacd559a27f957e5f8ca1450a637016c6f
SimonGideon <simongideon918@gmail.com>
2024-07-05 00:42:03 +0300

    Add Patient info aside section

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,3 @@
-// dom.js
 export const renderPatients = (data, container) => {
   data.forEach((patient) => {
     const { name, gender, age, profile_picture } = patient;
@@ -32,4 +31,90 @@ export const renderPatients = (data, container) => {
 
     $(container).append(patientDiv);
   });
+};
+
+export const renderPatientDetails = (patientData, container) => {
+  const patient = patientData.find(
+    (patient) => patient.name === "Jessica Taylor"
+  );
+
+  console.log("Patient Data:", patient);
+  const {
+    name,
+    profile_picture,
+    date_of_birth,
+    gender,
+    phone_number,
+    emergency_contact,
+    insurance_type,
+    lab_results,
+  } = patient;
+
+  $(container).empty();
+
+  const $patientInfo = $('<div class="col d-flex-col pat-info"></div>');
+
+  const $aside = $('<aside class="mb-5"></aside>');
+  $aside.append(`
+      <img class="pro-pic" src="${profile_picture}" alt="patient-profile">
+      <h2>${name}</h2>
+      <div class="align-self-start patient-more-details">
+        <div class="date_of_birth">
+          <img src="./assets/BirthIcon.png" alt="calendar">
+          <span>
+            <p>Date of Birth</p>
+            <p>${date_of_birth}</p>
+          </span>
+        </div>
+        <div class="gender">
+          <img src="./assets/FemaleIcon.png" alt="calendar">
+          <span>
+            <p>Gender</p>
+            <p>${gender}</p>
+          </span>
+        </div>
+        <div class="contact">
+          <img src="./assets/PhoneIcon.png" alt="contact">
+          <span>
+            <p>Contact Info.</p>
+            <p>${phone_number}</p>
+          </span>
+        </div>
+        <div class="emergency-contact">
+          <img src="./assets/PhoneIcon.png" alt="emergency-contact">
+          <span>
+            <p>Emergency Contact.</p>
+            <p>${emergency_contact}</p>
+          </span>
+        </div>
+        <div class="insurance-provider">
+          <img src="./assets/InsuranceIcon.png" alt="insurance-provider">
+          <span>
+            <p>Insurance Provider</p>
+            <p>${insurance_type}</p>
+          </span>
+        </div>
+      </div>
+      <button type="submit">Show All Information</button>
+    `);
+
+  const $labResults = $("<aside></aside>");
+  const $labsContainer = $(
+    `<div class="align-self-start d-flex-col lab-container w-100">`
+  );
+
+  $labsContainer.append("<h2>Lab Results</h2>");
+  lab_results.forEach((result) => {
+    $labsContainer.append(`
+        <div class="d-flex justify-content-between p-1 align-items-center w-100">
+          <p class="mb-0">${result}</p>
+          <img src="./assets/download.png" alt="download-button">
+        </div>
+    `);
+  });
+  $labResults.append($labsContainer);
+
+  // Appending both sections to the main container
+  $patientInfo.append($aside, $labResults);
+  $(container).append($patientInfo);
 };
--- index.js
+++ index.js
@@ -1,77 +1,15 @@
-// const username = "coalition";
-// const password = "skills-test";
-// const credentials = `${username}:${password}`;
-// const encodedCredentials = btoa(credentials);
-// const patientEntry = document.querySelector(".patients-list");
-// console.log(patientEntry);
-
-// const url = "https://fedskillstest.coalitiontechnologies.workers.dev";
-// const headers = {
-//   Authorization: `Basic ${encodedCredentials}`,
-//   "Content-Type": "application/json",
-// };
-
-// fetch(url, {
-//   method: "GET",
-//   headers: headers,
-// })
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log("API Data:", data);
-
-//     data.forEach((patient) => {
-//       const { name, gender, age, profile_picture } = patient;
-//       const patientDiv = document.createElement("div");
-//       patientDiv.classList.add("patient");
-
-//       const patientDetailsDiv = document.createElement("div");
-//       patientDetailsDiv.classList.add("d-flex", "gap-4", "patient-details");
-
-//       const avatar = document.createElement("img");
-//       avatar.classList.add("avatar");
-//       avatar.src = profile_picture;
-//       patientDetailsDiv.appendChild(avatar);
-
-//       const patientDetails = document.createElement("div");
-//       const nameP = document.createElement("p");
-//       nameP.classList.add("name");
-//       nameP.textContent = name;
-//       patientDetails.appendChild(nameP);
-
-//       const genderAgeSpan = document.createElement("span");
-//       genderAgeSpan.textContent = `${gender}, ${age} `;
-//       patientDetails.appendChild(genderAgeSpan);
-
-//       const actionBtn = document.createElement("img");
-//       actionBtn.classList.add("more-btn");
-//       actionBtn.src = "./assets/more_vert.png";
-//       actionBtn.alt = "More options";
-
-//       patientDetailsDiv.appendChild(patientDetails);
-//       patientDiv.appendChild(patientDetailsDiv);
-//       patientDiv.appendChild(actionBtn);
-//       patientEntry.appendChild(patientDiv);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("Error fetching data:", error);
-//   });
-
 import { fetchData } from "./patientService.js";
-import { renderPatients } from "./dashboard.js";
+import { renderPatients, renderPatientDetails } from "./dashboard.js";
 
 const url = "https://fedskillstest.coalitiontechnologies.workers.dev";
 const patientEntry = ".patients-list";
+const patientDetails = ".pat-info";
 
 fetchData(url)
   .done((data) => {
     console.log("API Data:", data);
     renderPatients(data, patientEntry);
+    renderPatientDetails(data, patientDetails);
   })
   .fail((error) => {
     console.error("Error fetching data:", error);
